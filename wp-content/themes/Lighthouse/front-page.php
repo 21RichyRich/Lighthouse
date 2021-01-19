9c5e6e9b054a2e075785952395c3fc42bf7af0fe
“Richmond <richmond.k.yeboah@gmail.com>
2021-01-18 21:05:58 -0500

    New theme with bootstrap and scss

--- wp-content/themes/Lighthouse/front-page.php
+++ wp-content/themes/Lighthouse/front-page.php
@@ -44,7 +44,9 @@
     <div class="container">
             <div class="card-deck">
         <div class="card">
-            <img class="card-images" src="..." alt="Card image cap">
+        <div class="card-pictures">
+        </div>
+            <img class="card-img-top" src="..." alt="Card image cap">
             <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
